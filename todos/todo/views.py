df161d67a70f101a02cee01e8eb90ff969bead94
hammer <ttkagentusolie@gmail.com>
2024-03-14 12:56:03 +0800

    Добавлены страницы задач

--- todos/todo/views.py
+++ todos/todo/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
+from .forms import TodoForm
+from .models import Todo
 
 
 def home(request):
@@ -68,4 +70,31 @@ def signupuser(request):
 
 
 def currenttodos(request):
-    return render(request, "todo/currenttodos.html")
+    todos = Todo.objects.all()
+    return render(request, "todo/currenttodos.html", {"todos": todos})
+
+
+def createtodo(request):
+    if request.method == "GET":
+        return render(request, "todo/createtodo.html", {"form": TodoForm()})
+    else:
+        try:
+            form = TodoForm(request.POST)
+            new_todo = form.save(commit=False)
+            new_todo.user = request.user
+            new_todo.save()
+            return redirect("currenttodos")
+        except ValueError:
+            return render(
+                request,
+                "todo/createtodo.html",
+                {
+                    "form": TodoForm(),
+                    "error": "Переданы не верные данные. Прпробуйте еще раз.",
+                },
+            )
+
+
+def viewtodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk=todo_pk)
+    return render(request, "todo/viewtodo.html", {"todo": todo})
